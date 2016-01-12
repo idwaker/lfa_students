c3bc806cf75986a5e9175b24d43f0fc0741b902e
Diwaker Ghimire <idwaker@gmail.com>
2016-01-12 11:04:01 +0545

    getting ready for heroku

--- students/settings.py
+++ students/settings.py
@@ -14,8 +14,8 @@ import os
 import dj_database_url
 
 # Parse database configuration from $DATABASE_URL
-#DATABASES = {}
-#DATABASES['default'] = dj_database_url.config()
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'students.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
