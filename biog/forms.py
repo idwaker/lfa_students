c6cecc291d72d4b1d4b15553cd0bd1b9666dc8b0
Diwaker Ghimire <idwaker@gmail.com>
2016-01-12 13:29:57 +0545

    fixed type

--- biog/forms.py
+++ biog/forms.py
@@ -22,14 +22,14 @@ class ProfileForm(forms.ModelForm):
                   'slideshare_links', 'youtube_links', 'additional_details',
                   'picture']
         widgets = {
-            'address': forms.TextArea(attrs={'rows': 2, 'cols': 40}),
-            'key_skills': forms.TextArea(attrs={'rows': 2, 'cols': 40}),
-            'additional_skills': forms.TextArea(attrs={'rows': 2, 'cols': 40}),
-            'about': forms.TextArea(attrs={'rows': 2, 'cols': 40}),
-            'linkedin_profile': forms.TextArea(attrs={'rows': 1, 'cols': 40}),
-            'github_projects': forms.TextArea(attrs={'rows': 3, 'cols': 40}),
-            'slideshare_links': forms.TextArea(attrs={'rows': 3, 'cols': 40}),
-            'youtube_links': forms.TextArea(attrs={'rows': 3, 'cols': 40}),
-            'additional_details': forms.TextArea(attrs={'rows': 3, 'cols': 40}),
+            'address': forms.Textarea(attrs={'rows': 2, 'cols': 40}),
+            'key_skills': forms.Textarea(attrs={'rows': 2, 'cols': 40}),
+            'additional_skills': forms.Textarea(attrs={'rows': 2, 'cols': 40}),
+            'about': forms.Textarea(attrs={'rows': 2, 'cols': 40}),
+            'linkedin_profile': forms.Textarea(attrs={'rows': 1, 'cols': 40}),
+            'github_projects': forms.Textarea(attrs={'rows': 3, 'cols': 40}),
+            'slideshare_links': forms.Textarea(attrs={'rows': 3, 'cols': 40}),
+            'youtube_links': forms.Textarea(attrs={'rows': 3, 'cols': 40}),
+            'additional_details': forms.Textarea(attrs={'rows': 3, 'cols': 40}),
         }
 
